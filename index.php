68fb4a32eaa65c159ac35a54e4eae57e918142b5
alannguyen2705 <55863320+alannguyen2705@users.noreply.github.com>
2021-06-03 16:43:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,24 @@
 			session_start();
 			include("admincp0101101000110011/config.php");
 		?>
+	<form method="POST" action="login.php">
+	<fieldset>
+	    <legend>Đăng nhập</legend>
+	    	<table>
+	    		<tr>
+	    			<td>Username</td>
+	    			<td><input type="text" name="username" size="30"></td>
+	    		</tr>
+	    		<tr>
+	    			<td>Password</td>
+	    			<td><input type="password" name="password" size="30"></td>
+	    		</tr>
+	    		<tr>
+	    			<td colspan="2" align="center"> <input type="submit" name="btn_submit" value="Đăng nhập"></td>
+	    		</tr>
+	    	</table>
+  </fieldset>
+  </form>
 
 </body>
 </html>
